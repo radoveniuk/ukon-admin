3fa304889cd6a0581cbb2771b497d7a24ca3a831
bohdan radoveniuk <bogdan.radoveniuk@gmail.com>
2026-02-24 17:45:49 +0100

    Add loghs page

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 const ROUTES = [
   { url: '/users', text: 'Používatelia' },
   { url: '/orders', text: 'Objednávky' },
+  { url: '/email-logs', text: 'Email logy' },
   { url: '/blog', text: 'Blog' },
 ];
 
@@ -24,10 +25,43 @@ const Header: React.FC = () => {
   return (
     <nav>
       <div style={{ display: 'flex', gap: 20 }}>
-        {ROUTES.map((route) => <Link key={route.url} href={route.url}><a className={isActive(route.url) && 'active'}>{route.text}</a></Link>)}
-        <Link key={'https://dash.bunny.net/storage'} href={'https://dash.bunny.net/storage'}><a target="_blank" style={{ color: '#717171' }}>Bunny</a></Link>
-        <Link key={'https://docs.google.com/spreadsheets/d/1oePCjFSRoI6pVVRPjL8I18FiiK-94kKCCT1dzXBlqQU/edit#gid=721786308'} href={'https://docs.google.com/spreadsheets/d/1oePCjFSRoI6pVVRPjL8I18FiiK-94kKCCT1dzXBlqQU/edit#gid=721786308'}><a target="_blank" style={{ color: '#717171' }}>CRM</a></Link>
-        <Link key={'https://docs.google.com/spreadsheets/d/1hzDcCeyk3gpDVBs_1WfaNn4L2qBREJJSaMP8yLQo898/edit#gid=156590612'} href={'https://docs.google.com/spreadsheets/d/1hzDcCeyk3gpDVBs_1WfaNn4L2qBREJJSaMP8yLQo898/edit#gid=156590612'}><a target="_blank" style={{ color: '#717171' }}>DocsGen</a></Link>
+        {ROUTES.map((route) => (
+          <Link key={route.url} href={route.url}>
+            <a className={isActive(route.url) && 'active'}>{route.text}</a>
+          </Link>
+        ))}
+        <Link
+          key={'https://dash.bunny.net/storage'}
+          href={'https://dash.bunny.net/storage'}
+        >
+          <a target="_blank" style={{ color: '#717171' }}>
+            Bunny
+          </a>
+        </Link>
+        <Link
+          key={
+            'https://docs.google.com/spreadsheets/d/1oePCjFSRoI6pVVRPjL8I18FiiK-94kKCCT1dzXBlqQU/edit#gid=721786308'
+          }
+          href={
+            'https://docs.google.com/spreadsheets/d/1oePCjFSRoI6pVVRPjL8I18FiiK-94kKCCT1dzXBlqQU/edit#gid=721786308'
+          }
+        >
+          <a target="_blank" style={{ color: '#717171' }}>
+            CRM
+          </a>
+        </Link>
+        <Link
+          key={
+            'https://docs.google.com/spreadsheets/d/1hzDcCeyk3gpDVBs_1WfaNn4L2qBREJJSaMP8yLQo898/edit#gid=156590612'
+          }
+          href={
+            'https://docs.google.com/spreadsheets/d/1hzDcCeyk3gpDVBs_1WfaNn4L2qBREJJSaMP8yLQo898/edit#gid=156590612'
+          }
+        >
+          <a target="_blank" style={{ color: '#717171' }}>
+            DocsGen
+          </a>
+        </Link>
       </div>
       <style jsx>{`
         nav {
